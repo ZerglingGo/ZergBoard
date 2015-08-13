d44b17c949ab3524fb4aedc26a539fddb96628ae
ZerglingGo <zerglinggo@zerglinggo.net>
2015-08-13 15:24:17 +0900

    Initialize

---
+++ class.ZergBoard.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * PHP Board System
+ * 
+ * PHP 게시판 시스템
+ * 
+ *  author ZerglingGo <zerglinggo@zerglinggo.net>
+ *  GitHub http://github.com/ZerglingGo/ZergBoard
+ * license http://opensource.org/licenses/MIT MIT License
+ */
+
+
+?>
